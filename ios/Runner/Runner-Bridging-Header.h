0977b47ec89776259007a2114479012bcab04e06
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-17 17:26:27 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,4 +1,3 @@
-#import <NodeMobile/NodeMobile.h>
-
-// 如果 NodeMobile.h 中没有 node_start 声明，手动添加
+#import "GeneratedPluginRegistrant.h"
+#import "NodeJSManager.h"
 int node_start(int argc, char *argv[]);
