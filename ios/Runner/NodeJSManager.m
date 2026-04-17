a8c010e39e2f578ff282f568d3b82c8cab0cd90c
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-17 17:27:21 +0800

    Create NodeJSManager.m

---
+++ ios/Runner/NodeJSManager.m
@@ -0,0 +1,72 @@
+#import "NodeJSManager.h"
+#import <NodeMobile/NodeMobile.h>
+
+@interface NodeJSManager ()
+@property (nonatomic, assign) BOOL isRunning;
+@property (nonatomic, assign) int nodeServerPort;
+@end
+
+@implementation NodeJSManager
+
++ (instancetype)shared {
+    static NodeJSManager *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[NodeJSManager alloc] init];
+    });
+    return instance;
+}
+
+- (void)startNodeJSWithScriptPath:(NSString *)scriptPath completion:(void (^)(BOOL))completion {
+    if (self.isRunning) {
+        if (completion) completion(YES);
+        return;
+    }
+    
+    const char *argv[] = {"node", [scriptPath UTF8String], NULL};
+    int argc = 2;
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        int result = node_start(argc, (char **)argv);
+        self.isRunning = NO;
+        NSLog(@"Node.js exited with code %d", result);
+    });
+    
+    // 等待初始化
+    [NSThread sleepForTimeInterval:2.0];
+    self.isRunning = YES;
+    if (completion) completion(YES);
+}
+
+- (void)stopNodeJS {
+    // NodeMobile 无 node_stop 函数，仅标记
+    self.isRunning = NO;
+}
+
+- (void)sendMessage:(NSString *)message completion:(void (^)(id _Nullable, NSError * _Nullable))completion {
+    if (self.nodeServerPort == 0) {
+        if (completion) completion(nil, [NSError errorWithDomain:@"NodeJS" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Node service port unknown"}]);
+        return;
+    }
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://127.0.0.1:%d/msg", self.nodeServerPort]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = [message dataUsingEncoding:NSUTF8StringEncoding];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (completion) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completion(nil, error);
+            });
+        }
+    }];
+    [task resume];
+}
+
+- (void)setNodeServerPort:(int)port {
+    self.nodeServerPort = port;
+}
+
+@end
